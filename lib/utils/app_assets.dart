42632cd96951a4cac823257a29c0c5169a6f3ea6
youssefmohamed192 <Daowlly1@gmail.com>
2024-03-04 13:14:47 +0200

    Initial commit

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,10 @@
+abstract class AppAssets{
+  static const String imagePath = "assets/images/";
+  static const String splash = "${imagePath}splash.png";
+  static const String ballImage = "${imagePath}ball.png";
+  static const String businessImage = "${imagePath}business.png";
+  static const String envImage = "${imagePath}environment.png";
+  static const String healthImage = "${imagePath}health.png";
+  static const String politicsImage = "${imagePath}politics.png";
+  static const String scienceImage = "${imagePath}science.png";
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+  static const Color primary = Color(0xff39A552);
+  static const Color accent = Color(0xff4F5A69);
+  static const Color red = Color(0xffC91C22);
+  static const Color blue = Color(0xff003E90);
+  static const Color purple = Color(0xffED1E79);
+  static const Color brown = Color(0xffCF7E48);
+  static const Color lightBlue = Color(0xff4882CF);
+  static const Color yellow = Color(0xffF2D352);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color transparent = Colors.transparent;
+  static const Color description = Color(0xff42505C);
+  static const Color time = Color(0xffA3A3A3);
+  static const Color title = Color(0xff79828B);
+  static const Color black = Colors.black;
+}
